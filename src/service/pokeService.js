c539c6b8748a946b7dc9aa468a9f0f3b4f37d1b5
Shpakova Alena <shpakovadev@gmail.com>
2020-05-31 09:18:05 +0300

    add pokeService

---
+++ src/service/pokeService.js
@@ -0,0 +1,26 @@
+export default class pokeService {
+  _nameUrl = 'https://pokeapi.co/api/v2'
+
+  getAnswer = async url => {
+    const res = await fetch(`${this._nameUrl}${url}`)
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`)
+    }
+    return await res.json()
+  }
+
+  getAllPokemons = async () => {
+    const res = await this.getAnswer(`/pokemon/`)
+    return res
+  }
+
+  getPokemon = async id => {
+    const details = await this.getAnswer(`/pokemon/${id}/`)
+    return details
+  }
+
+  detAbilityPokemon = async id => {
+    const ability = await this.getAnswer(`/ability/${id}/`)
+    return ability
+  }
+}
